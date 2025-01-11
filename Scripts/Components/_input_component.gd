d49eb7d57559a1bfe6dddc057f47c9e61983ac4f
e-war <elliotwarren17@gmail.com>
2025-01-11 17:02:57 +0000

    added turret + shot controller

--- Scripts/Components/_input_component.gd
+++ Scripts/Components/_input_component.gd
@@ -1,13 +1,20 @@
 extends Node
 
+signal begin_firing
+signal end_firing
+signal shot_fired(strength: int)
+
 @export_category("Settings")
 @export var controls: CharacterBody2D
-@export var max_speed: float = 15.0
-@export var fall_speed: float = 29.8
-var is_falling: bool
+@export var max_speed: float = 30.0
+@export var fall_speed: float = 50.0
+
+var is_firing: bool
+var firing_tween: Tween
+var current_fire_strength: int
 
 func gravity() -> void:
-	''' applies gravity to characterbody2d controls accelerate if has been falling for x time '''
+	''' applies gravity to characterbody2d controls ?accelerate if has been falling for x time? '''
 	
 	if(!floor_check()):
 		controls.velocity.y = fall_speed
@@ -17,13 +24,57 @@ func gravity() -> void:
 func floor_check() -> bool:
 	return controls.is_on_floor()
 
-func _process(delta) -> void:
+func movement_controls() -> void:
 	''' take in the left/right direction, apply appropriate velocity + gravity'''
+	
 	var input_direction = Input.get_axis("left","right")
 	
 	gravity()
 	
-	if(input_direction != 0):
+	if(input_direction != 0 and !is_firing):
 		controls.velocity.x = input_direction * max_speed
 	else:
 		controls.velocity.x = lerpf(controls.velocity.x,0,.2)
+
+func turret_controls(d) -> void:
+	var turret_direction = Input.get_axis("up","down")
+	
+	if(turret_direction != 0):
+		controls.turret.rotation = lerp_angle(controls.turret.rotation, 1.55 * turret_direction, .5 *d)
+
+func fire_controls() -> void:
+	''' holding down fire button will cause a ui component to show, indicating the shot strength
+	this strength will increase from 0 - 100 and back for as long as is held
+	
+	Once released the shot will be fired, once the last projectile has exploded the player will no longer be able to move
+	and their turn will be over.
+	 '''
+	
+	var trigger_fire: bool = Input.is_action_just_pressed("fire")
+	var trigger_release: bool  = Input.is_action_just_released("fire")
+	
+	if(trigger_fire and !is_firing):
+		#triggered fire and not already firing
+		is_firing = true
+		emit_signal("begin_firing")
+	if(trigger_release and is_firing):
+		#began firing and released
+		is_firing = false
+		emit_signal("end_firing")
+		emit_signal("shot_fired",controls.shoot_progress) # bind projectile shot to this
+		
+
+func _begin_shooting_animation():
+	firing_tween = create_tween()
+	firing_tween.set_loops()
+	firing_tween.tween_property(controls.shoot_progress,"value",100,1)
+	firing_tween.tween_property(controls.shoot_progress,"value",0,1)
+
+func _end_shooting_animation():
+	firing_tween.kill()
+	controls.shoot_progress.value = 0
+
+func _process(delta) -> void:
+	movement_controls()
+	turret_controls(delta)
+	fire_controls()
--- Scripts/Player/tank.gd
+++ Scripts/Player/tank.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
+@export_category("tank_parts")
+@export var body: Sprite2D
+@export var turret: Sprite2D
+
+@export_category("tank_ui")
+@export var shoot_progress: TextureProgressBar
+
 func _physics_process(delta):
 	move_and_slide()
